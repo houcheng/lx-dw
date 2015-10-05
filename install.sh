0c72a40a364667434e4bbef32bee00bb7c906f89
Houcheng Lin <houcheng@gmail.com>
2015-10-05 22:40:13 +0800

    add install.sh and lx-dw command
    Signed-off-by: Houcheng Lin <houcheng@gmail.com>

---
+++ install.sh
@@ -0,0 +1,3 @@
+cp lx-dw /usr/bin/
+cp lx-dw.py /usr/bin/
+echo "copy lx-dw and lx-dw.py into /usr/bin/"
--- lx-dw.sh
+++
@@ -1,4 +0,0 @@
-xterm -fa 'Monospace' -fs 12 -e "python lx-dw.py bt-dw" &
-xterm -fa 'Monospace' -fs 12 -e "python lx-dw.py reg-dw" &
-xterm -fa 'Monospace' -fs 12 -e "python lx-dw.py watch-dw" &
-
